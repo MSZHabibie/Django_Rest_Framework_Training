1ce4a6cf9390541985a306eab53be269ab7132a6
mszhabibie <mszhabibie@gmail.com>
2022-07-04 03:21:34 +0700

    finally i reach the end of this API Basic

--- djrfproject/djrfproject/views.py
+++ djrfproject/djrfproject/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
 from djrfapp.serializers import StudentSerializer
 from djrfapp.models import Student
 
 class TesView(APIView):
+
+   permission_classes = (IsAuthenticated, )
+
    def get(self, request, *args, **kwargs):
       queryset = Student.objects.all()
-      serializer = StudentSerializer(queryset, many=True)
+      student1 = queryset.first()
+      serializer = StudentSerializer(student1)
       return Response(serializer.data)
 
    def post(self, request, *args, **kwargs):
